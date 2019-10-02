b33709ae70ccb8c3939a45ba3caf66d9117addde
kylianvc <kylianvancanneyt@gmail.com>
2019-10-02 19:49:07 +0200

    commentaar

--- src/scripts/Detail.js
+++ src/scripts/Detail.js
@@ -3,6 +3,7 @@
 //     console.log(naam);
 // }
 
+// Moet id nemen van de film waarop geklikt is en die na movie zetten
 export function getDetail() {
     axios.get("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=4bc044ddb757b980e41e9e71ef3aafae").then(function(response){
     getDetail(response.data)
--- src/scripts/Overzicht.js
+++ src/scripts/Overzicht.js
@@ -3,6 +3,7 @@
 //     console.log(naam);
 // }
 
+// Zorg dat er op de cards kan geklikt worden
 export function getOverzicht() {
     axios.get("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=4bc044ddb757b980e41e9e71ef3aafae");
 }
