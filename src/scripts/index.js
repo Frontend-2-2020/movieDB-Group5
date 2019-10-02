d1e6b83c9d197211b7ac00a17ba7f2a75bf69184
Tom Derdaele <t.derdaele@goekint.be>
2019-10-02 18:59:08 +0200

    aanpassingen css

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,16 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/index.scss';
 import axios from 'axios';
-import query-string from 'query-string';
+import queryString from 'query-string';
 import {} from "./Overzicht";
 import {} from "./Detail";
 
 // als key toont detail geen key overzichtpagina
-if () {
-    axios.get("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=4bc044ddb757b980e41e9e71ef3aafae");
-} else {
+// if () {
+//     axios.get("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=4bc044ddb757b980e41e9e71ef3aafae");
+// } else {
     
-}
+// }
 
 // const number = Math.random();
 // if (number > 0.5) {
