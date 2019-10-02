55080653793e862921977002bb9d65772534a947
kylianvc <kylianvancanneyt@gmail.com>
2019-10-02 19:42:27 +0200

    split werkt

--- src/scripts/Overzicht.js
+++ src/scripts/Overzicht.js
@@ -3,6 +3,6 @@
 //     console.log(naam);
 // }
 
-function getOverzicht() {
+export function getOverzicht() {
     axios.get("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=4bc044ddb757b980e41e9e71ef3aafae");
 }
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,6 +6,20 @@ import {} from "./Overzicht";
 import {} from "./Detail";
 
 // als key toont detail geen key overzichtpagina
+
+const parsed = queryString.parse(window.location.search);
+console.log(parsed);
+
+if (parsed.movie) {
+    //movie uit url
+    var show = document.getElementById("overzicht");
+    show.classList.add("toondetail");
+}
+else{
+    var show2 = document.getElementById("detail");
+    show2.classList.add("toonoverzicht");
+}
+
 // if () {
 //     axios.get("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=4bc044ddb757b980e41e9e71ef3aafae");
 // } else {
