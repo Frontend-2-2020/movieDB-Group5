fc6f3815704c85d9d381a48877b88f0598bb1d52
kylianvc <kylianvancanneyt@gmail.com>
2019-10-03 18:05:49 +0200

    site met afbeeldingen

--- src/scripts/Overzicht.js
+++ src/scripts/Overzicht.js
@@ -15,6 +15,7 @@ maak de klikfunctie
 
 */
 
+// haalt data op uit de databank
 var movies = [];
 export function getOverzicht() {
     Axios.get("https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=4bc044ddb757b980e41e9e71ef3aafae")
@@ -26,6 +27,7 @@ export function getOverzicht() {
     
 };
 
+// Vult de cards in met de data
 
 function showCards() {
     var html = "";
@@ -33,7 +35,7 @@ function showCards() {
         const data = movies[i];
         html = html + `<div class="col-md-6 col-lg-3 mt-4">
                 <div class="card">
-                    <img src="https://api.themoviedb.org/3/discover/movie?sort_b…ity.desc&api_key=4bc044ddb757b980e41e9e71ef3aafae${data.poster_path}" class="card-img-top" alt="...">
+                    <img src="https://image.tmdb.org/t/p/w154/${data.poster_path}" class="card-img-top" alt="...">
                     <div class="card-body">
                     <h5 class="card-title">${data.title}</h5>
                     <p class="card-text">${data.vote_average}</p>
@@ -52,6 +54,7 @@ function showCards() {
     var container = document.querySelector("#overzichtCards");
         container.innerHTML=html
 };
+
 
 
 
