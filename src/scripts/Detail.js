b65e9de3cf48e3cae262c9e2434fd106e14412d2
Tom Derdaele <t.derdaele@goekint.be>
2019-10-06 13:07:44 +0200

    Basis van detailpagina werkt. Enkel nog wat opmaak aanpassen.

--- src/scripts/Detail.js
+++ src/scripts/Detail.js
@@ -1,59 +1,61 @@
 import Axios from "axios";
 
 
-// export function initMark(){
-//     const naam = "Mark";
-//     console.log(naam);
-// }
+let singlemovie = [];
+let productiehuisLijst = [];
 
-// Moet id nemen van de film waarop geklikt is en die na movie zetten
-// export function getDetail() {
-//     axios.get("https://api.themoviedb.org/3/movie/429617?api_key=4bc044ddb757b980e41e9e71ef3aafae").then(function(response){
-//     getDetail(response.data)
-   
-//     })
-// }
-
-//  console.log(getDetail);
-
-
-var singlemovie = [];
 export function getDetail(id) {
     Axios.get("https://api.themoviedb.org/3/movie/"+id+"?api_key=4bc044ddb757b980e41e9e71ef3aafae")
     .then(function (response) {
         singlemovie=response.data;
+        productiehuisLijst=singlemovie.production_companies;
         detailInfo();
-        console.log(singlemovie);  
+        addhouse();
     });
     
 };
 
 
 function detailInfo() {
-    var htmlDetail = "";
-    // for (var i = 0; i < singlemovie.length; i++) {
-    //     const data = singlemovie[i];
-    //     htmlDetail = htmlDetail + 
+    let htmlDetail = "";
 
     htmlDetail +=   `<div class="card card__single">
-            <img src="/public/placeholder.jpg" class="card-img-top" alt="...">
+            <img src="https://image.tmdb.org/t/p/w500/${singlemovie.poster_path}" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">${singlemovie.title}</h5>
               <p class="card-text">${singlemovie.vote_average}</p>
               <p class="card-text">${singlemovie.release_date}</p>
-              <p>Samenvatting</p>
-              <p>Productie Bedrijven</p>
+              <p class="card-text">${singlemovie.overview}</p>
+              <ul id="productieHuizen"></ul>
             </div>
         </div>
     </div>`
 
-
-
-        // container.innerHTML(
-            
-        // )
-        
-    // }
-    var containerDetail = document.querySelector("#detail");
+    const containerDetail = document.querySelector("#detail");
         containerDetail.innerHTML=htmlDetail
 };
+
+function addhouse(){
+    let htmlHouse = "";
+
+    for (const productiehuisnaam of productiehuisLijst) {
+        // console.log(productiehuisnaam.name);
+
+        htmlHouse += `<li class="productiehuis">${productiehuisnaam.name}</li>`
+
+        const productie = document.querySelector("#productieHuizen");
+            productie.innerHTML=htmlHouse;
+      };
+};
+
+
+// OUDE manier (gewoon om het beter te begrijpen :) )
+// function addhouse(){
+//     for (const productiehuisnaam of productiehuisLijst) {
+//         console.log(productiehuisnaam.name);
+//         const productie = document.querySelector("#productieHuizen");
+//         const litje = document.createElement("li");
+//         litje.appendChild(document.createTextNode(productiehuisnaam.name))
+//         productie.appendChild(litje);
+//       };
+// };
